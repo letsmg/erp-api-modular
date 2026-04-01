eec4542ef5d5beed1c6484f28e842b506db7fcee
Luiz Eduardo <luiztsilva@protonmail.com>
2026-04-01 10:57:47 -0300

    fix: restaura ícone personalizado do projeto modular
    - Mantém icon.ico como favicon do projeto modular
    - Remove favicon.ico padrão para usar ícone personalizado
    - Cada projeto agora tem seu próprio ícone distinto

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('icon.ico') }}">
 
     @if (!app()->environment('testing'))
         @vite(['resources/js/app.ts'])
